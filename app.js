68f9fa500e8c501a7b1adb0a53ae53a849fe1af8
francismeng <francismeng@xwfintech.com>
2020-09-17 11:29:09 +0800

    change app

--- app.js
+++ app.js
@@ -1,61 +1,43 @@
+var http = require('http')
+var createHandler = require('github-webhook-handler')
 const spawn = require('child_process').spawn
-const http = require('http')
-const createHandler = require('github-webhook-handler')
-const handler = createHandler([
-  {
-    path: '/webhook/react-admin',
-    secret: 'react-admin'
-  },
-  {
-    path: '/webhook/react-admin-node',
-    secret: 'react-admin-node'
-  },
-  {
-    path: '/webhook/gatsby-blog',
-    secret: 'gatsby-blog'
-  }
-])
-http.createServer(function(req, res){
-  handler(req, res, function(err) {
+var handler = createHandler({ path: '/webhook/gatsby-blog', secret: 'gatsby-blog' })
+
+http.createServer(function (req, res) {
+  handler(req, res, function (err) {
     res.statusCode = 404
     res.end('no such location')
   })
 }).listen(7777)
 
-handler.on('error', (err) => {
-  console.error(err)
+handler.on('error', function (err) {
+  console.error('Error:', err.message)
 })
 
-handler.on('push', e => {
-  console.log(
-    'Received a push event for %s to %s',
+handler.on('push', function (event) {
+  console.log('Received a push event for %s to %s',
     event.payload.repository.name,
-    event.payload.ref,
-    e.payload,
-    e
-  )
-  switch(e.path){
-    case '/webhook/react-admin':
-      runCmd('sh', ['./sh/react-admin.sh', event.payload.repository.name], function (text) { console.log(text) })
-      break
-    case '/webhook/react-admin-node':
-      runCmd('sh', ['./sh/react-admin-node.sh', event.payload.repository.name], function (text) { console.log(text) })
-      break
-    case '/webhook/gatsby-blog':
-      runCmd('sh', ['./sh/gatsby-blog.sh', event.payload.repository.name], function (text) { console.log(text) })
-      break
-    default: 
-      break
-  }
+    event.payload.ref)
+    try {
+      const s = spawn('sh', ['./sh/gatsby-blog.sh'], {
+        cwd: `../${e.payload.repository.name}`
+      })
+      s.stdout.on('data', (data) => {
+        console.log(`${e.payload.repository.name}: ${data}`);
+      })
+      s.stderr.on('data', (data) => {
+        console.log(`${e.payload.repository.name}: ${data}`);
+      });
+      console.log(e.payload.repository.name, 'has rebuild');
+      } catch (e) {
+          console.log(e)
+      }
 })
 
-function runCmd(cmd, args, callback){
-  const child = spawn(cmd, args)
-  let resp = ''
-  child.stdout.on('data', (buffer) => {
-    resp += buffer
-  })
-  child.stdout.on('end', () => {
-    callback(resp)
-  })
-}
+handler.on('issues', function (event) {
+  console.log('Received an issue event for %s action=%s: #%d %s',
+    event.payload.repository.name,
+    event.payload.action,
+    event.payload.issue.number,
+    event.payload.issue.title)
+})
