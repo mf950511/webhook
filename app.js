a890e8fe013f0e7477d0b11a3cdccab48ba78ca9
francismeng <francismeng@xwfintech.com>
2020-09-29 14:16:20 +0800

    调整静态文件默认目录

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ function runCmd (cmd, args, callback) {
 
 
 app.
-use(koaBody({ "formLimit":"5mb", "jsonLimit":"5mb", "textLimit":"5mb" })).]
+use(koaBody({ "formLimit":"5mb", "jsonLimit":"5mb", "textLimit":"5mb" })).
 use(serve(path.resolve(__dirname, '../../website/website-home'))).
 use(serve(path.resolve(__dirname, '../../website/blog'))).
 use(serve(path.resolve(__dirname, '../../website/react-admin'))).
